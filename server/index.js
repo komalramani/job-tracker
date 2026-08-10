164c258fd1ce2858ccc0e87918ff48f729a14a2e
Komal Ramani <komalramani@yahoo.com>
2026-08-10 17:11:45 +0800

    Connect backend to PostgreSQL database

--- server/index.js
+++ server/index.js
@@ -1,38 +1,47 @@
-const cors=require("cors");
+const cors = require("cors");
 const express = require("express");
+const pool = require("./db");
+
 const app = express();
 const PORT = 3001;
+
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Job Tracker API is running");
 });
-let applications = [
-  {
-    id: 1,
-    company: "Google",
-    role: "Software Engineer",
-    status: "Applied",
-  },
-];
 
-app.get("/applications", (req, res) => {
-  res.json(applications);
+// GET from PostgreSQL
+app.get("/applications", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM applications ORDER BY id"
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
-app.post("/applications", (req, res) => {
-  console.log("POST received:", req.body);
+// POST to PostgreSQL
+app.post("/applications", async (req, res) => {
+  try {
+    const { company, role, status } = req.body;
 
-  const newApplication = {
-    id: applications.length + 1,
-    company: req.body.company,
-    role: req.body.role,
-    status: req.body.status,
-  };
+    const result = await pool.query(
+      `INSERT INTO applications (company, role, status)
+       VALUES ($1, $2, $3)
+       RETURNING *`,
+      [company, role, status]
+    );
 
-  applications.push(newApplication);
-
-  res.status(201).json(newApplication);
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 app.listen(PORT, () => {
