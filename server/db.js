6c1d6760f5205779702f4117bf6cca2f4a31ed80
Komal Ramani <komalramani@yahoo.com>
2026-08-19 15:41:32 +0800

    Configure production database connection

--- server/db.js
+++ server/db.js
@@ -1,13 +1,21 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database:
-  process.env.NODE_ENV === "test"
-    ? process.env.DB_TEST_NAME
-    : process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: false,
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database:
+          process.env.NODE_ENV === "test"
+            ? process.env.DB_TEST_NAME
+            : process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 
 module.exports = pool;
